82e4d1642c048332d6b0342ae448bc2b140fcb04
benjamincrook <crookbenj@gmail.com>
2025-11-17 16:53:04 +0000

    Up to lecture 3

--- src/uk/ac/cam/bjc76/supohw/Supo1/ExampleClass.java
+++ src/uk/ac/cam/bjc76/supohw/Supo1/ExampleClass.java
@@ -1,4 +1,9 @@
-package Supo1;
+package uk.ac.cam.bjc76.supohw.Supo1;
 
 public class ExampleClass {
+    public final String name;
+
+    public ExampleClass (String name) {
+        this.name = name;
+    }
 }
--- src/uk/ac/cam/bjc76/supohw/Supo1/Question2.java
+++ src/uk/ac/cam/bjc76/supohw/Supo1/Question2.java
@@ -1,12 +1,11 @@
-package Supo1;
+package uk.ac.cam.bjc76.supohw.Supo1;
 
 public class Question2 {
     public static void main (String[] args) {
         int aPrimitive = 100;
+//      aReference is the reference that points to an object ExampleClass("Ben"),
+//      which is an instance of the class ExampleClass
+        ExampleClass aReference = new ExampleClass("Ben");
 
     }
-}
-
-public class MyClass {
-    MyClass 
 }
